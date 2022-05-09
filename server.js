39a457e0952dac69511741c84b95e3f7dc53f3bb
Suraj325 <suraj1503kain@gmail.com>
2022-05-09 20:17:27 +0530

    ejs file added

--- server.js
+++ server.js
@@ -3,45 +3,38 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
 const path = require("path");
-const cors = require('cors');
-const cookieParser = require('cookie-parser')
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-
-
-const connectDB = require('./server/database/connection.js');
+const connectDB = require("./server/database/connection.js");
 
 const app = express();
 app.use(cors());
 
-dotenv.config({ path: '.env' });
-const PORT = process.env.PORT || 8080
-
+dotenv.config({ path: ".env" });
+const PORT = process.env.PORT || 8080;
 
 // Log request Using Morgan
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
 // mongodb connection
 connectDB();
 
 // Parse request  to body parser
-app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 app.use(cookieParser());
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // set view engine
-app.set("view engin", "ejs");
-app.set('views', path.join(__dirname, '/views'))
-
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 // Load routers
-app.use('/user',require('./server/routes/userRoutes.js'))
-app.use('/loan',require('./server/routes/loanRoutes.js'))
-
+app.use("/user", require("./server/routes/userRoutes.js"));
+app.use("/loan", require("./server/routes/loanRoutes.js"));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
-
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
