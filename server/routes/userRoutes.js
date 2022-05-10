ecb3965e7b65985fcbc35a7249523d398e3f32de
Suraj325 <suraj1503kain@gmail.com>
2022-05-10 11:17:20 +0530

    make request from browser for signup and login

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,18 +1,44 @@
 const express = require("express");
-const controller = require('../controller/userController.js')
-const userAuth = require('../middleware/userAuth')
-const {upload} = require('../middleware/upload')
+const controller = require("../controller/userController.js");
+const userAuth = require("../middleware/userAuth");
+const { upload } = require("../middleware/upload");
+const services = require("../services/render");
 
 const route = express.Router();
 
+// Services
+route.get("/login", services.userLogin);
+route.get("/register", services.userSignup);
+route.get("/apply", services.userLoanApply);
+
 //APIs
 
-route.post('/register',controller.Register);
-route.post('/login',controller.Login);
+route.post("/register", controller.Register);
+route.post("/login", controller.Login);
 
-route.post('/uploadProfilePhoto',userAuth,upload.single('profilePhoto'),controller.uploadProfilePhoto);
-route.post('/uploadAadhar',userAuth,upload.single('Aadhar'),controller.uploadAdhar);
-route.post('/uploadPan',userAuth,upload.single('panCard'),controller.uploadPan);
-route.post('/uploadSalarySlips',userAuth,upload.array('SalarySlips'),controller.uploadSalarySlips);
+route.post(
+  "/uploadProfilePhoto",
+  userAuth,
+  upload.single("profilePhoto"),
+  controller.uploadProfilePhoto
+);
+route.post(
+  "/uploadAadhar",
+  userAuth,
+  upload.single("Aadhar"),
+  controller.uploadAdhar
+);
+route.post(
+  "/uploadPan",
+  userAuth,
+  upload.single("panCard"),
+  controller.uploadPan
+);
+route.post(
+  "/uploadSalarySlips",
+  userAuth,
+  upload.array("SalarySlips"),
+  controller.uploadSalarySlips
+);
 
 module.exports = route;
